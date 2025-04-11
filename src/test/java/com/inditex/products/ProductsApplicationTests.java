48d7e5b2dbb35470ad747af392f3519dbe38b5f5
rubenhernandez <rubenhernandez@paradigmadigital.com>
2025-04-11 16:01:04 +0200

    Init Spring boot project

---
+++ src/test/java/com/inditex/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
